c8f97e07103ae79fdc5165469c637475a06bec03
mattia-pellin <mattia@pellin.it>
2026-02-23 09:23:47 +0100

    Fix LFI login endpoint targeting ?do=login

--- backend/crawlers/impl/italian_sites.py
+++ backend/crawlers/impl/italian_sites.py
@@ -36,6 +36,25 @@ class LaForestaIncantataCrawler(DLECrawler):
         except Exception:
             pass
 
+    async def login(self) -> bool:
+        if not self.username or not self.password:
+            return False
+            
+        # Get homepage for cookies
+        await self.session.get(self.base_url)
+        
+        login_data = {
+            "login_name": self.username,
+            "login_password": self.password,
+            "login": "submit",
+            "login_not_save": "1"
+        }
+        
+        # LFI requires targeting the ?do=login endpoint specifically
+        login_url = f"{self.base_url.rstrip('/')}/index.php?do=login"
+        res = await self.session.post(login_url, data=login_data, headers={"Referer": self.base_url})
+        return self.username.lower().split('@')[0] in res.text.lower()
+
 class HD4MeCrawler(DLECrawler):
     name = "hd4me"
     base_url = "https://hd4me.net/"
